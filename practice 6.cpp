80e7fb712573a227e2d416e8699bd406622f929c
TON618OFF <rusblackgod@gmail.com>
2024-04-28 15:08:48 +0300

    Добавьте файлы проекта.

---
+++ practice 6.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Student 
+{
+    public:
+        string name;
+        string group;
+        string lesson;
+        int done_works;
+        int fail_works;
+
+        Student(string name, string group, string lesson, int done_works, int fail_works) :
+            group(group),
+            lesson(lesson),
+            done_works(done_works),
+            fail_works(fail_works) 
+        {
+
+        }
+};
+
+class Teacher 
+{
+    public:
+        string name;
+        string group;
+        string lesson;
+
+        Teacher(string name, string group, string lesson) :
+            name(name),
+            group(group),
+            lesson(lesson) 
+        {
+
+        }
+};
+
+class MPT 
+{
+    
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
