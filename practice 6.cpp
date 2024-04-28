1b3af65b593618928ee80a5beea9b94b6276713a
TON618OFF <rusblackgod@gmail.com>
2024-04-28 21:07:05 +0300

    update 2.0

--- practice 6.cpp
+++ practice 6.cpp
@@ -22,31 +22,30 @@ public:
 
 
 class Teacher {
-    public:
-        string name;
-        vector<string> groups;
-        vector<string> lessons;
+public:
+    string name;
+    vector<string> groups;
+    vector<string> lessons;
 
-        Teacher(string name, vector<string> groups, vector<string> lessons) :
-            name(name),
-            groups(groups),
-            lessons(lessons) {}
+    Teacher(string name, vector<string> groups, vector<string> lessons) :
+        name(name),
+        groups(groups),
+        lessons(lessons) {}
 
-        void add_lesson_to_teacher(string lesson) {
-            lessons.push_back(lesson);
-        }
+    void add_lesson_to_teacher(string lesson) {
+        lessons.push_back(lesson);
+    }
 
-        void add_group_to_teacher(string group) {
-            groups.push_back(group);
-        }
+    void add_group_to_teacher(string group) {
+        groups.push_back(group);
+    }
 };
+
 
 class MPT {
 public:
     vector<Student> students;
     vector<Teacher> teachers;
-
-
 
     void add_student_to_group(Student student, string group) {
 
@@ -137,8 +136,50 @@ public:
         }
     }
 
+    void print_attendance(int num_present, int num_total) {
+
+        if (num_present < 0) {
+            cout << "Количество присутствующих студентов не может быть отрицательным" << endl;
+            return;
+        }
+        if (num_present > num_total) {
+            cout << "Количество присутствующих студентов не может быть больше общего количества студентов в группе" << endl;
+            return;
+        }
+
+        if (num_present == 0) {
+            cout << "На паре нет студентов" << endl;
+        }
+        else if (num_present == 1) {
+            cout << "На паре присутствует 1 студент" << endl;
+        }
+        else {
+            cout << "На паре присутствует " << num_present << " студентов из " << num_total << endl;
+        }
+    }
+
+    void remove_student_from_group(Student student, string group) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students[i].name == student.name) {
+                students.erase(students.begin() + i);
+                return;
+            }
+        }
+        cout << "Студент не найден" << endl;
+    }
+
+    void check_students(vector<Student>& students) {
+        for (Student& student : students) {
+            if (student.fail_works > 5) {
+                cout << "У студента больше 5 задолженностей! Он был удалён из группы!" << endl;
+                remove_student_from_group(student, student.group);
+            }
+        }
+    }
+
 
 };
+
 
 
 
@@ -181,6 +222,9 @@ int main() {
     college.add_lesson_to_teacher(teacher1, "Информатика");
     college.add_group_to_teacher(teacher1, "П50-3-22");
 
+    
+    college.check_students(college.students);
+
     cout << "\n\n\nПреподаватели:" << endl;
     cout << "Имя: " << teacher1.name << endl;
     cout << "Дисциплины: ";
@@ -191,8 +235,18 @@ int main() {
     cout << "Группы: ";
     for (string group : teacher1.groups) {
         cout << group << " ";
-    }
+    }   
     cout << endl;
+
+    int num_present;
+    int num_total;
+    cout << "\n\n\nВведите количество присутствующих студентов: " << endl;
+    cin >> num_present;
+    cout << "Введите общее количество студентов в группе: " << endl;
+    cin >> num_total;
+
+    cout << "Информация о присутствующих на паре: " << endl;
+    college.print_attendance(num_present, num_total);
 
     return 0;
 }
